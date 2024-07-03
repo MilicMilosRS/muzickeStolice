8b09b1dd699ecfddc9d86cd3b217efcd852a036b
MilicMilosRS <milicmilos.rs@gmail.com>
2024-07-03 14:29:34 +0200

    [ADD] Database controller

---
+++ muzickeStolice/Controller/DatabaseController.cs
@@ -0,0 +1,14 @@
+﻿using muzickeStolice.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace muzickeStolice.Controller
+{
+    public static class DatabaseController
+    {
+        public static AppDbContext database = new AppDbContext();
+    }
+}
